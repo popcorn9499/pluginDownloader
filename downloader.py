0ca52163ed2f614e5e176ab1d8502f97f8f92266
popcorn9499 <popcorn9499@gmail.com>
2017-07-04 15:38:11 -0500

    added creating the config
    Default config is now created by the program itself

--- downloader.py
+++ downloader.py
@@ -5,6 +5,24 @@ import os
 
 tempDir = "./temp/"
 
+def createConfig():
+    config={
+    "data": {
+        "1": {
+            "fileName": "downloaded file name",
+            "url": "any random site that doesnt do weird download formatting for its downloads",
+            "type": "none"
+        },
+        "2": {
+            "fileName": "downloaded file name",
+            "url": "https://api.github.com/repos/(owner_name)/(repo_name)/releases/latest",
+            "type": "github",
+            "file": "github latest release file name"
+        }
+    }
+}
+    fileSave(config.json,config)
+
 ##file load and save stuff
 
 def fileSave(fileName,config):
@@ -28,29 +46,30 @@ def download(url,fileName,directory): #downloads the file given
 
 
 
-config = fileLoad("config.json") #loads the config file
 
-#check if the temp directory exists
-if os.path.isdir(tempDir) == False:
-    os.mkdir(tempDir)
-
-
-mainData = config["data"]
-
-for key,val in mainData.items(): #Looking through the data dictionary and cycling through it
-    if val["type"] == "github": #if its a file from github where the latest release downloads the json formatting for stuff
-        r = requests.get(val["url"])
-        data = r.json() #outputs the format as json
-        for key in data["assets"]: #sorts through the json till it finds the download that matches the file we want
-            if key["name"] == val["file"]:
-                download(key["browser_download_url"],val["fileName"],tempDir)
-        #fileSave(val["fileName"],data["assets"])
-    else: #regular download
-        download(val["url"],val["fileName"],tempDir)
-    source="{0}{1}".format(tempDir,val["fileName"])
-    destination="Z:\\User\\Documents\\My_Files\\Development_Stuff\\Python\\downloadTests\\downloaded\\{0}".format(val["fileName"])
-    if os.path.isfile(destination) == True: #checks to see if the file already exists
-        os.remove(destination)#if so delete the file
-    os.rename(source,destination)#moves the output file to the correct location.
+if os.path.isfile("config.json") == False: #check if the config file exists or not
+    createConfig() #create config
+    print("program exitting please put proper information in the config")
+else: #run program if config exists
+    config = fileLoad("config.json") #loads the config file
+    #check if the temp directory exists
+    if os.path.isdir(tempDir) == False:
+        os.mkdir(tempDir)
+    mainData = config["data"]
+    for key,val in mainData.items(): #Looking through the data dictionary and cycling through it
+        if val["type"] == "github": #if its a file from github where the latest release downloads the json formatting for stuff
+            r = requests.get(val["url"])
+            data = r.json() #outputs the format as json
+            for key in data["assets"]: #sorts through the json till it finds the download that matches the file we want
+                if key["name"] == val["file"]:
+                    download(key["browser_download_url"],val["fileName"],tempDir)
+            #fileSave(val["fileName"],data["assets"])
+        else: #regular download
+            download(val["url"],val["fileName"],tempDir)
+        source="{0}{1}".format(tempDir,val["fileName"])
+        destination="Z:\\User\\Documents\\My_Files\\Development_Stuff\\Python\\downloadTests\\downloaded\\{0}".format(val["fileName"])
+        if os.path.isfile(destination) == True: #checks to see if the file already exists
+            os.remove(destination)#if so delete the file
+        os.rename(source,destination)#moves the output file to the correct location.
 
         
